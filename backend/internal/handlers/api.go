9ac2c50096009e883c8f695bd03575f8762238e3
Ryan Mello <ryanmello897@gmail.com>
2025-03-11 21:30:02 -0700

    remove comments

--- backend/internal/handlers/api.go
+++ backend/internal/handlers/api.go
@@ -7,11 +7,7 @@ import (
 	chimiddle "github.com/go-chi/chi/middleware"
 )
 
-// the 'H' tells the complier that the function can be exported
-// a lowercase letter tells the complier that the function is private and can't be exported
 func Handler(r * chi.Mux){
-	// removes trailing slashes from the url
-	// trailing slahes lead to a 404 error
 	r.Use(chimiddle.StripSlashes)
 	
 	r.Route("/account", func(router chi.Router){
