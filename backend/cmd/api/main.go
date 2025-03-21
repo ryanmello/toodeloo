0a7fbca9208ef1d4b1b84d46bb03f4983ce1f89b
Ryan Mello <ryanmello897@gmail.com>
2025-03-21 08:45:51 -0700

    basic http server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"toodeloo/internal/handlers"
-
-	"github.com/go-chi/chi"
-	log "github.com/sirupsen/logrus"
 )
 
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Helo from the server"))
+}
+
 func main() {
-	log.SetReportCaller(true)
-	var r *chi.Mux = chi.NewRouter()
-
-	handlers.Handler(r)
-
-	fmt.Println("Starting GO API service...")
-
-	err := http.ListenAndServe("localhost:8080", r)
-	if err != nil {
-		log.Error(err)
+	s := &server{addr: ":8080"}
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
 	}
 }
