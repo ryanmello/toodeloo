6a4b0f169c41f80d831bddf4538fe1d2bf21cede
Ryan Mello <ryanmello897@gmail.com>
2025-03-26 07:56:33 -0700

    chi mux

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,15 +17,18 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
 
-	return mux
+	return r
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux *chi.Mux) error {
 	srv := &http.Server{
 		Addr:         app.config.addr,
 		Handler:      mux,
