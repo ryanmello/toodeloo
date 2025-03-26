e3d6eaca8910c7fe21f157d05022d546ce2c62ef
Ryan Mello <ryanmello897@gmail.com>
2025-03-26 08:20:26 -0700

    api setup

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -17,9 +17,15 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *chi.Mux {
+func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
@@ -28,7 +34,7 @@ func (app *application) mount() *chi.Mux {
 	return r
 }
 
-func (app *application) run(mux *chi.Mux) error {
+func (app *application) run(mux http.Handler) error {
 	srv := &http.Server{
 		Addr:         app.config.addr,
 		Handler:      mux,
