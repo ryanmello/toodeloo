dfe2b5e9c019ca468364bf710515e0a848a99855
Ryan Mello <ryanmello897@gmail.com>
2025-03-27 16:11:04 -0700

    post struct

--- backend/internal/store/posts.go
+++ backend/internal/store/posts.go
@@ -5,6 +5,17 @@ import (
 	"database/sql"
 )
 
+type Post struct {
+	Id        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserId    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+	Version   int      `json:"version"`
+}
+
 type PostStore struct {
 	db *sql.DB
 }
