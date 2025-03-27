db0ab093dd3521aba32a908b90d0289fbbb2b6a9
Ryan Mello <ryanmello897@gmail.com>
2025-03-27 16:02:02 -0700

    define store in app

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"toodeloo/internal/env"
+	"toodeloo/internal/store"
 
 	"github.com/joho/godotenv"
 )
@@ -17,10 +18,14 @@ func main() {
 		addr: addr,
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 
 	mux := app.mount()
+
 	log.Fatal(app.run(mux))
 }
