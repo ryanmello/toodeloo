4212da798b0930b8835af120dc1e266d1539d7b7
Ryan Mello <ryanmello897@gmail.com>
2025-03-28 16:08:22 -0700

    db

---
+++ backend/internal/db/db.go
@@ -0,0 +1,12 @@
+package db
+
+import "database/sql"
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, err
+	}
+	
+	return db, nil
+}
