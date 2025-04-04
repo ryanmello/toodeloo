90ae0f61c33ebe8a5f8d984302f660dba3261910
Ryan Mello <ryanmello897@gmail.com>
2025-04-04 16:11:05 -0700

    standardize json response

--- backend/cmd/api/json.go
+++ backend/cmd/api/json.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
 	"github.com/go-playground/validator/v10"
+	"net/http"
 )
 
 var Validate *validator.Validate
@@ -34,4 +34,12 @@ func writeJSONError(w http.ResponseWriter, status int, message string) error {
 	}
 
 	return writeJSON(w, status, &envelope{Error: message})
+}
+
+func (app *application) jsonResponse(w http.ResponseWriter, status int, data any) error {
+	type envelope struct {
+		Data any `json:"data"`
+	}
+
+	return writeJSON(w, status, &envelope{ Data: data })
 }
